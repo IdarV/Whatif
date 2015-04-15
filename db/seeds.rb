c3ec21470664ce602da009b17de9645fd9fcb060
IdarV <tjoida13@nith.no>
2015-04-15 17:14:36 +0200

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Question.create(total_taken: 0, yes: 0, no: 0, whatif: 'something', but: 'somethingelse')
+
+
+User.create(name: 'Vidar', answered: 0, common:0.0, picture: 'http://th04.deviantart.net/fs70/PRE/i/2012/218/c/0/buckethead_by_stonedsour887-d4w3x92.jpg')
