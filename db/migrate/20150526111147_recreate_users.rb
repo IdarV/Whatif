0bca84b32944687e2d2af59fd4fb7ef0b809cf9f
IdarV <tjoida13@nith.no>
2015-05-26 13:34:21 +0200

    added device functionality

---
+++ db/migrate/20150526111147_recreate_users.rb
@@ -0,0 +1,12 @@
+class RecreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.integer :answered
+      t.decimal :common
+      t.string :picture
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526102536) do
+ActiveRecord::Schema.define(version: 20150526111943) do
 
   create_table "questions", force: :cascade do |t|
     t.integer  "total_taken"
@@ -22,5 +22,27 @@ ActiveRecord::Schema.define(version: 20150526102536) do
     t.string   "whatif"
     t.string   "but"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "answered"
+    t.decimal  "common"
+    t.string   "picture"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
