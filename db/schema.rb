bf4864c8f92ebf51735fb4e71f6e1d7d7f0c814c
IdarV <tjoida13@nith.no>
2015-05-26 12:34:21 +0200

    partly added device, removed users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411144447) do
+ActiveRecord::Schema.define(version: 20150526102536) do
 
   create_table "questions", force: :cascade do |t|
     t.integer  "total_taken"
@@ -21,15 +21,6 @@ ActiveRecord::Schema.define(version: 20150411144447) do
     t.datetime "updated_at",  null: false
     t.string   "whatif"
     t.string   "but"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "answered"
-    t.decimal  "common"
-    t.string   "picture"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
