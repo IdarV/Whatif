94c4b3f66609741ed12d78b9f1a1b2d9a469e60a
IdarV <tjoida13@nith.no>
2015-05-27 20:11:58 +0200

    changed randomquestion layout; added buttons and made post route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  post '/randomquestion' => 'questions#randomquestion'
+
   # You can have the root of your site routed with "root"
   # root 'users#signin'
   devise_scope :user do
